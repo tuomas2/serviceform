865814899ea6cfd2ef7084d63fda098b024c8127
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2017-05-22 15:27:41 +0300

    Add setup.py etc.

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.in') as requirements_file:
+    requirements = requirements_file.read()
+
+
+setup(
+    name="serviceform",
+    version="0.9.3",
+    description="Django web application to collect volunteering willingness "
+                "data and report to responsible people",
+    long_description=readme,
+    author="Tuomas Airaksinen",
+    author_email="tuomas.airaksinen@gmail.com",
+    url="https://github.com/tuomas2/serviceform",
+    packages=['serviceform.'+i for i in find_packages('serviceform')],
+    include_package_data=True,
+    package_data={
+        '': ["*.html", "*.css", "*.scss", "fontawesome*", "*.js"],
+    },
+    install_requires=[
+        "Django",
+        "django-libsass",
+        "psycopg2",
+        "django-nested-admin",
+        "django-crispy-forms",
+        "django-colorful",
+        "colour",
+        "django-redis-cache",
+        "django-compressor",
+        "django-guardian",
+        "django-select2-forms",
+    ],
+    extras_require={
+        'cachalot': ['django-cachalot'],
+        'grappelli': ['django-grappelli'],
+        'celery': ['django-celery', 'celery'],
+    },
+    license="GPL",
+    zip_safe=False,
+    classifiers=[
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
