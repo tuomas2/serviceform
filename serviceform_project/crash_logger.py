6e724dc828f70f06e4369e904ad9ba1ff9c2616e
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2017-05-24 09:55:01 +0300

    Docstring

--- serviceform_project/crash_logger.py
+++ serviceform_project/crash_logger.py
@@ -4,6 +4,9 @@ class LogException(Exception):
     pass
 
 class CrashHandler(Handler):
+    """
+    This is being used for tests to crash on error level log events.
+    """
     def emit(self, record):
         msg = self.format(record)
         raise LogException(msg)
