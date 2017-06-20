7f8b377e2011ace456be7ca303455089c9902399
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2017-06-20 19:13:00 +0300

    Fix version complaint

--- serviceform/__init__.py
+++ serviceform/__init__.py
@@ -1,3 +1,6 @@
 import pkg_resources
 
-__version__ = pkg_resources.get_distribution("serviceform").version
+try:
+    __version__ = pkg_resources.get_distribution("serviceform").version
+except pkg_resources.DistributionNotFound:
+    __version__ = 'not installed'
